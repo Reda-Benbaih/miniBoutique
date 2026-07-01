8da2e5a3cf294385de91ba9cfff0a7e298e9b106
Reda-benbaih <wareda145@gmail.com>
2026-07-01 10:41:58 +0200

    first commit

---
+++ boutique/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section>
+        <Navbar/>
+      </section>
+    </>
+  )
+}
+
+export default App
